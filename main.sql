af1ec8424d05be328203efc686d03ccaa548e2e1
Gnlow <gnlow@pm.me>
2025-07-09 23:48:29 +0900

    dissolve

--- main.sql
+++ main.sql
@@ -5,3 +5,11 @@ select road_rank, count(distinct road_name)
     from data
     group by road_rank
 ;
+
+.width 3, 20
+select road_no as '#', road_name
+    from data
+    where road_rank = 101
+    group by road_name
+    order by cast(road_no as integer)
+;
